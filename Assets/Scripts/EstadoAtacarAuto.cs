e8e9aea46dfb630ef2ced50cb1334952e02c7743
DerekCarmiol <derek.carmiol@gmail.com>
2025-08-11 16:13:07 -0600

    Recolección de recursos y texturas
    aplicación de textura a los edificios y recursos (recolección de los recursos y destrucción)

--- Assets/Scripts/EstadoAtacarAuto.cs
+++ Assets/Scripts/EstadoAtacarAuto.cs
@@ -4,6 +4,7 @@ public class EstadoAtacarAuto : IEstadoUnidadJugador
 {
     public void Ejecutar(UnidadMilitar unidad)
     {
+        // Primero, atacar enemigos cercanos
         EnemigoIA[] enemigos = GameObject.FindObjectsOfType<EnemigoIA>();
 
         foreach (var enemigo in enemigos)
@@ -12,8 +13,23 @@ public class EstadoAtacarAuto : IEstadoUnidadJugador
 
             if (distancia < 4f)
             {
-                Debug.Log(unidad.tipoUnidad + " ataca automáticamente a " + enemigo.name);
-                enemigo.RecibirDanio(10); // si querés, implementamos vida para enemigos también
+                enemigo.RecibirDanio(10);
+                Debug.Log(unidad.tipoUnidad + " atacó a un enemigo.");
+                return;
+            }
+        }
+
+        // Si no hay enemigos, buscar recursos
+        RecursoMineral[] recursos = GameObject.FindObjectsOfType<RecursoMineral>();
+
+        foreach (var recurso in recursos)
+        {
+            float distancia = Vector3.Distance(unidad.transform.position, recurso.transform.position);
+
+            if (distancia < 2f)
+            {
+                recurso.RecibirDanio(10);
+                Debug.Log(unidad.tipoUnidad + " recolecta recurso.");
                 return;
             }
         }
